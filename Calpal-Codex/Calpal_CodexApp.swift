6f734641ed3a2ee02845019b34ed4dafd72c9739
Xuantong Yan <xuantongyan@gmail.com>
2026-03-27 13:49:35 -0700

    Initial Commit

---
+++ Calpal-Codex/Calpal_CodexApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calpal_CodexApp.swift
+//  Calpal-Codex
+//
+//  Created by Xuantong Yan on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct Calpal_CodexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calpal-Codex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calpal-Codex
+//
+//  Created by Xuantong Yan on 3/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
